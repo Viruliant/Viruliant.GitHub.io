6a5ef55f99ef7ff123c13d97b45283a44c374c88
GlassGhost <roypfund@gmail.com>
2015-01-15 21:15:15 -0600

    Signed-off-by: GlassGhost <roypfund@gmail.com>

--- gh-template-node.js
+++ gh-template-node.js
@@ -1,12 +1,13 @@
+//you may have to install nodejs & run 'npm install http-server' to get this to run
+var	httpServer = require('http-server');
+
+var server = httpServer.createServer({});
+server.listen(1337);
+
 //var http = require('http');
 //http.createServer(function (request, response) {
 //	response.writeHead(200, {'Content-Type': 'text/plain'});
 //	response.end('Hello World\n');
 //}).listen(1337, '127.0.0.1');
 //console.log('Server running at http://127.0.0.1:1337/');
-
-var	httpServer = require('http-server');
-
-var server = httpServer.createServer({});
-server.listen(1337);
 
